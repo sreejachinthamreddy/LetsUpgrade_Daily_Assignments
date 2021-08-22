f42cd36a498f76e98330957453f307774f17ebf8
19121A0544 <78897938+sreejachinthamreddy@users.noreply.github.com>
2021-08-22 15:09:40 +0530

    Add files via upload

---
+++ string frequency count.py
@@ -0,0 +1,11 @@
+# Python3 code to demonstrate
+# each occurrence frequency using
+# collections.Counter()
+from collections import Counter
+
+# initializing string
+test_str = input("enter name")
+res = Counter(test_str)
+
+print(str(res))
+
